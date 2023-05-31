af451dc1181977697ea7be414f0f6b2bad2bd153
torobucci <kevintoro254@gmail.com>
2023-05-31 16:02:09 +0300

    load books and people

--- main.rb
+++ main.rb
@@ -2,6 +2,8 @@ require_relative './app'
 require_relative './options'
 def main
   app = App.new
+  app.load_books
+  app.load_people
   puts 'Welcome to the School Library App!'
   options(app)
 end
