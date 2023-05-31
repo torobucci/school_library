e8edb7e2ee257d99177232521dceb5a4d5fdd691
torobucci <kevintoro254@gmail.com>
2023-05-31 18:24:13 +0300

    update to and from json methods

--- rental.rb
+++ rental.rb
@@ -8,4 +8,31 @@ class Rental
     @person = person
     person.rentals << self
   end
+
+  def to_json(*args)
+    {
+      'date' => @date,
+      'book' => @book.to_json,
+      'person' => @person.to_json
+    }.to_json(*args)
+  end
+
+  def self.from_json(json_data, books, people)
+    rental_data = JSON.parse(json_data)
+    book = find_book_by_title(JSON.parse(rental_data['book'])['title'], books)
+    person = find_person_by_id(JSON.parse(rental_data['person'])['id'], people)
+
+    Rental.new(rental_data['date'], book, person)
+  end
+
+  private
+
+  def self.find_book_by_title(title, books)
+    books.find { |book| book.title == title }
+  end
+
+  def self.find_person_by_id(id, people)
+    people.find { |person| person.id == id.to_i }
+  end
+
 end
